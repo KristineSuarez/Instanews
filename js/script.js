93e62fa2260b989bc4663d6652916ec2ffabb0e2
Kristine Suarez <suarezkristine_001@live.ca>
2019-10-24 15:02:56 -0700

    jQuery

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
+// let tmpSubject;
 
+document.addEventListener('DOMContentLoaded', function (event){
+    $('#link').on('change', function(){
+        event.preventDefault();
+        // tmpSubject = $(this).val();
+        $.ajax({
+            method: 'GET',
+            // dataType: 'JSON',
+            url: 'https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=VBGBP5ATc6Rgv8j8nrYEdyH1zVlNDA8U' 
+        })
+        .done(function(data){
+            console.log(data.results);
+            let container = $('#article-list')
+            $.each(data.results, function(key, value) {
+                console.log(value)
+                $('#article-list').append
+            })
+        })
+    })
+});
 

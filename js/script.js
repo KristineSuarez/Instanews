be9b1ed72e05a9653a28b0df2f3e4117a66a8d4d
Kristine Suarez <suarezkristine_001@live.ca>
2019-10-25 16:28:16 -0700

    SASS update

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
-// let tmpSubject;
+let tmpSubject;
 
 document.addEventListener('DOMContentLoaded', function (event){
     $('#link').on('change', function(){
         event.preventDefault();
-        // tmpSubject = $(this).val();
+        tmpSubject = $(this).val();
         $.ajax({
             method: 'GET',
-            // dataType: 'JSON',
-            url: 'https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=VBGBP5ATc6Rgv8j8nrYEdyH1zVlNDA8U' 
+            dataType: 'JSON',
+            url: 'https://api.nytimes.com/svc/topstories/v2/'+ tmpSubject+'.json?api-key=VBGBP5ATc6Rgv8j8nrYEdyH1zVlNDA8U' 
         })
         .done(function(data){
+            console.clear();
             console.log(data.results);
             let container = $('#article-list')
             $.each(data.results, function(key, value) {
-                console.log(value)
+                console.log(value.multimedia[4]).url
                 $('#article-list').append
             })
         })

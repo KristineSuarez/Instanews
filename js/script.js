e468aa7c062fb8976bf44f59b88733b748d27e46
Kristine Suarez <suarezkristine_001@live.ca>
2019-10-29 16:50:53 -0700

    API update

--- js/script.js
+++ js/script.js
@@ -10,30 +10,40 @@ document.addEventListener('DOMContentLoaded', function (event){
             url: 'https://api.nytimes.com/svc/topstories/v2/'+ tmpSubject+'.json?api-key=VBGBP5ATc6Rgv8j8nrYEdyH1zVlNDA8U' 
         })
         .done(function(data){
-            console.clear();
-            console.log(data.results);
-            let container = $('#article-list')
-            $.each(data.results, function(key, value) {
-                console.log(value.multimedia[4].url);
-                if(value.multimedia[0]==undefined || value.multimedia[0]==null){
-                    console.log("Hello");
-                }
-                console.log(key);
-                $('#article-list').append(`
+            console.log(data);
+            // $('#article-list').slice(0, 12);
+            // const results = data.results.slice(0, 12);
+            // $('#article-list').empty();
 
-                
-                    <li>
-                    <div style="background-image:url(${value.multimedia[4].url})">   
-                    <a href="">
+
+            const slicedResults = data.results.slice(0,12);
+
+            console.clear();
+            // console.log(data.results);
+            let container = $('#article-list')
+            $.each(slicedResults, function(key, value) {
+                // console.log(value.multimedia[4].url);
+                if(value.multimedia[4]==undefined || value.multimedia[4]==null){
+                    console.log("undefine or empty multimedia");
+                    return;
+                }
+                console.log(value.multimedia[4].url);
+                $('#article-list').append(`
+                    <li class="article-list-item" style="background-image:url(${value.multimedia[4].url})">
+                    
+                    <a href="${value.url}" target="_blank">
                             <p>${value.abstract}</p>    
                         </a>
-                       </div>
+                      
                     </li>
                 `);
-
+                // ${value.abstract}
                 
             });// .each
         })
     })
 });
 
+
+            // $(this).timeout=3000;
+            // $(data.results).slice(0,12);

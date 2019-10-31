2f078193b49b3ca05022548b4fa9c92a37b85993
Kristine Suarez <suarezkristine_001@live.ca>
2019-10-31 16:58:28 -0700

    Media Query Update

--- js/script.js
+++ js/script.js
@@ -11,33 +11,31 @@ document.addEventListener('DOMContentLoaded', function (event){
         })
         .done(function(data){
             console.log(data);
-            // $('#article-list').slice(0, 12);
-            // const results = data.results.slice(0, 12);
-            // $('#article-list').empty();
 
+            const slicedResults = data.results.filter(function(articleObj){
+                return articleObj.multimedia[4] !== undefined;
+            }).slice(0,12);
 
-            const slicedResults = data.results.slice(0,12);
+            $('.article-list').empty();
 
-            console.clear();
-            // console.log(data.results);
             let container = $('#article-list')
             $.each(slicedResults, function(key, value) {
                 // console.log(value.multimedia[4].url);
-                if(value.multimedia[4]==undefined || value.multimedia[4]==null){
-                    console.log("undefine or empty multimedia");
-                    return;
-                }
-                console.log(value.multimedia[4].url);
-                $('#article-list').append(`
+                // if(value.multimedia[4]==undefined || value.multimedia[4]==null){
+                //     console.log("undefine or empty multimedia");
+                //     return;
+                // }
+                // console.log(value.multimedia[4].url);
+                $('.article-list').append(`
                     <li class="article-list-item" style="background-image:url(${value.multimedia[4].url})">
-                    
-                    <a href="${value.url}" target="_blank">
-                            <p>${value.abstract}</p>    
-                        </a>
-                      
+                        <a class="article-page" href="${value.url}" target="_blank">
+                            <p class="article">
+                                ${value.abstract}
+                            </p> 
+                        </a> 
                     </li>
                 `);
-                // ${value.abstract}
+                
                 
             });// .each
         })
@@ -45,5 +43,3 @@ document.addEventListener('DOMContentLoaded', function (event){
 });
 
 
-            // $(this).timeout=3000;
-            // $(data.results).slice(0,12);
